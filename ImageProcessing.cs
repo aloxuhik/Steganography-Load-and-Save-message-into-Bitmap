b3857205b84d85ff53aab192ee80435cf3318bb0
aloxuhik <45300644+aloxuhik@users.noreply.github.com>
2019-03-20 15:02:47 +0100

    Add files via upload

---
+++ ImageProcessing.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace Steganography
+{
+    class ImageProcessing
+    {    
+        public static string LoadMessage(Bitmap image)
+        {
+            Color color = image.GetPixel(0, 0);
+
+            int xStep = color.R;
+            int yStep = color.G;
+            int count = color.B;
+            int index = 0;
+
+            List<byte> data = new List<byte>();
+
+            int x = xStep;
+            int y = yStep;
+            while(index < count)
+            {
+                Color pixelData = image.GetPixel(x, y);
+                x += xStep;
+                y += yStep;
+                if (x >= image.Width)
+                    x = x - image.Width;
+                if (y >= image.Height)
+                    y = y - image.Height;
+                data.Add(pixelData.R);
+                data.Add(pixelData.G);
+                data.Add(pixelData.B);
+                index+=3;
+            }
+            return (Encoding.ASCII.GetString(data.ToArray()));
+        }
+        public static Bitmap SaveImage(Bitmap Image,string Message)
+        {
+            Bitmap image = (Bitmap)Image.Clone();
+
+            byte[] data = Encoding.ASCII.GetBytes(Message);
+
+            int xStep = image.Width / 3;
+            if (xStep > 255)
+                xStep = 255;
+            int yStep = image.Height / data.Length;
+            if (yStep > 255)
+                yStep = 255;
+
+            image.SetPixel(0, 0, Color.FromArgb(xStep, yStep, data.Length));
+
+            int index = 0;
+            int x = xStep;
+            int y = yStep;
+            while(index < data.Length)
+            {
+                if(index +2 < data.Length)
+                image.SetPixel(x, y, Color.FromArgb(data[index], data[index + 1], data[index + 2]));
+                else if (index+1<data.Length)
+                    image.SetPixel(x, y, Color.FromArgb(data[index], data[index + 1], 255));
+                else
+                    image.SetPixel(x, y, Color.FromArgb(data[index], 255, 255));
+
+                x += xStep;
+                y += yStep;
+                if (x >= image.Width)
+                    x = x - image.Width;
+                if (y >= image.Height)
+                    y = y - image.Height;
+                index += 3;
+            }
+            return (image);
+        }
+    }
+}
